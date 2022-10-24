2a1b55dafa983cae4d557c0481da181654f0dbc7
Ackzel John Borero <111472859+ackzel-06@users.noreply.github.com>
2022-10-24 12:12:23 +0800

    Create Exercise.java

---
+++ Exercise.java
@@ -0,0 +1,97 @@
+// import javax.print.event.PrintJobListener;
+
+// public class App {
+//     public static void main(String[] args) throws Exception {
+
+//          char [][] uno = {{'1','1','1'},{'1','1','1'},{'1','1','1'}, {'1', '1', '1'}};
+
+//         // for (int rows= 0; rows <uno.length; rows++){
+
+//         //     System.out.println("");
+
+//         //     for (int column = 0; column <3; column++){
+
+//         //         if (rows  %2 ==0){
+
+//         //             System.out.print(uno[rows][column]);
+
+
+//         //         }
+//         //     }
+//         // }
+//         // // PATTERN 2 
+//         //     System.out.println("");
+//         //     System.out.println("PATTERN 2");
+
+//         //         //OUTER LOOP
+//         //         for(int row = 0; row < uno.length; row++){
+//         //             System.out.println("");
+
+//         //         //INNER LOOP
+//         //         for (int column = 0; column < 3; column++) {
+
+//         //             if (row %2 ==0){
+//         //                 System.out.print(uno[row][column]);
+//         //             } else if (row %2 !=0){
+//         //                     System.out.print("x");
+//         //                 }
+                    
+//                 //   }
+// //         }// PATTERN 3
+
+// //         System.out.println("");
+
+// //         System.out.println("3. PATTERN 3");
+// //         // outer loop
+// //         for (int row = 0; row < uno.length; row++) {
+// //             System.out.println("");
+// //             // inner loop
+// //             for (int column = 0; column < 3; column++) { if (row == 0 || row == 3){
+// //                     System.out.print(uno[row][column]);
+// //                 }
+// //                     if (row == 1 || row == 2){
+// //                         if (column == 0 || column == 2){
+// //                             System.out.print(" ");
+// //                         } else {
+// //                           System.out.print(uno[row][column]);
+// //                         }
+// //                     }
+// //                 }  
+// //             }
+// //         }
+// // }
+//          //pattern 4
+// //     for (int row = 0; row < uno.length; row++) {
+
+// //         System.out.println(" ");
+
+// //         System.out.print("row " + row + ": ");
+
+// //     for (int column = 0; column < 3; column++) {
+
+// //         if(row == 0 || row == 2)
+
+// //           if (column == 0 || column == 2)
+
+// //           { System.out.print("  ");}
+// //           else
+// //           { System.out.print(uno[row][column] + " ");}
+// //         if (row == 1 || row ==3)
+// //         System.out.print(uno[row][column] + " ");
+// //         }
+// //     }
+// // }
+
+//  // }
+// // int [][] two  = {{1,1,1},{1,1,1},{1,1,1}, {1, 1, 1}};
+// //                  int sum=0;
+// //                     for (int rows= 0; rows <4; rows++){
+// //                         System.out.println("");
+// //                             for (int column = 0; column <3; column++){
+// //                                 sum += two[rows][column];        }
+// //       }
+// //       System.out.println("THE SUM IS: " + sum);
+// //     }
+// // }
+               
+       
